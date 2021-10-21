385c8bfcbcf629f39d18f908409f92ec70061959
harshitmarmat <you@example.com>
2021-10-21 21:16:50 +0530

    list updated

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,16 @@
 import { ADD_MOVIES } from "../actions";
 
-export default function movies(state = [], action){
+const intialMovieList = {
+    list : [],
+    favourite : []
+}
+
+export default function movies(state = intialMovieList, action){
     if(action.type === ADD_MOVIES){
-        return action.movies;
+        return {
+            ...intialMovieList,
+            list : action.movies
+        }
     } 
     return state;
 }
